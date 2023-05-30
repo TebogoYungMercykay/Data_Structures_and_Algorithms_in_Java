3751f83edc280a76bb035a58fb560db9d60ef837
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-05-31 01:21:29 +0200

    CombSort Implementation

--- Codes/SortingAlgorithms/CombSort.java
+++ Codes/SortingAlgorithms/CombSort.java
@@ -2,19 +2,18 @@ public class CombSort extends Sorting {
     public CombSort(){
         this.name = "CombSort";
     }
-    
     @Override
     public String[] sortAcs(Vertex[] array){
         int n_iterator = array.length;
         int gap_iterator = n_iterator;
         boolean swapped = false;
-        while(gap_iterator != 1 || swapped == true){
+        while (gap_iterator != 1 || swapped == true){
             swapped = false;
-            if(gap_iterator>1){
+            if (gap_iterator>1){
                 gap_iterator=(int)(gap_iterator/1.3);
             }
-            for(int i_iterator=0; i_iterator+gap_iterator<n_iterator; i_iterator++){
-                if(array[i_iterator].getVName().compareTo(array[i_iterator+gap_iterator].getVName())>0){
+            for (int i_iterator=0; i_iterator+gap_iterator<n_iterator; i_iterator++){
+                if (array[i_iterator].getVName().compareTo(array[i_iterator+gap_iterator].getVName())>0){
                     Vertex temp = array[i_iterator];
                     array[i_iterator] = array[i_iterator+gap_iterator];
                     array[i_iterator+gap_iterator] = temp;
@@ -26,19 +25,18 @@ public class CombSort extends Sorting {
         }
         return vertexToString(array);
     }
-
     @Override
     public String[] sortDsc(Vertex[] array) {
         int n_iterator = array.length;
         int gap_iterator = n_iterator;
         boolean swapped = false;
-        while(gap_iterator != 1 || swapped == true){
+        while (gap_iterator != 1 || swapped == true){
             swapped = false;
-            if(gap_iterator>1){
+            if (gap_iterator>1){
                 gap_iterator=(int)(gap_iterator/1.3);
             }
-            for(int i_iterator=0; i_iterator+gap_iterator<n_iterator; i_iterator++){
-                if(array[i_iterator].getVName().compareTo(array[i_iterator+gap_iterator].getVName())<0){
+            for (int i_iterator=0; i_iterator+gap_iterator<n_iterator; i_iterator++){
+                if (array[i_iterator].getVName().compareTo(array[i_iterator+gap_iterator].getVName())<0){
                     Vertex temp = array[i_iterator];
                     array[i_iterator] = array[i_iterator+gap_iterator];
                     array[i_iterator+gap_iterator] = temp;
